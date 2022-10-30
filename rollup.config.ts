9dd6fc41b2689fb2a47345452d3e8131ebdbf7d3
IIIMADDINIII <martin@elend.name>
2022-10-30 08:15:15 +0100

    Projectstructure

---
+++ rollup.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "rollup";
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from "@rollup/plugin-terser";
+
+export default defineConfig({
+  input: "./src/index.ts",
+  output: [{
+    file: "./dist/index.cjs",
+    format: "commonjs",
+    sourcemap: "inline",
+  }, {
+    file: "./dist/index.mjs",
+    format: "esm",
+    sourcemap: "inline",
+  }],
+  external: ["comlink", "electron"],
+  plugins: [typescript({ noEmitOnError: true }), nodeResolve(), terser()],
+});
